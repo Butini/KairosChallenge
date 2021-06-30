e47c197acb0522684df153a8a34f82033f3eae42
Butini <marcbiedmabutier@gmail.com>
2021-06-30 22:29:10 +0200

    Finish project. Window with button URL redirect. #4

--- KairosChallenge/frmUrlLink.Designer.cs
+++ KairosChallenge/frmUrlLink.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.infoTxt = new System.Windows.Forms.Label();
+            this.urlBtn = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // infoTxt
+            // 
+            this.infoTxt.AutoSize = true;
+            this.infoTxt.Location = new System.Drawing.Point(29, 24);
+            this.infoTxt.Name = "infoTxt";
+            this.infoTxt.Size = new System.Drawing.Size(184, 13);
+            this.infoTxt.TabIndex = 0;
+            this.infoTxt.Text = "Si pulsas el botón te llevara a la URL:";
+            // 
+            // urlBtn
+            // 
+            this.urlBtn.Location = new System.Drawing.Point(551, 67);
+            this.urlBtn.Name = "urlBtn";
+            this.urlBtn.Size = new System.Drawing.Size(75, 23);
+            this.urlBtn.TabIndex = 1;
+            this.urlBtn.Text = "¡Vamos!";
+            this.urlBtn.UseVisualStyleBackColor = true;
+            this.urlBtn.Click += new System.EventHandler(this.urlBtn_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 96);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1123, 444);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // frmUrlLink
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1148, 552);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.urlBtn);
+            this.Controls.Add(this.infoTxt);
+            this.Name = "frmUrlLink";
             this.Text = "frmUrlLink";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label infoTxt;
+        private System.Windows.Forms.Button urlBtn;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
--- KairosChallenge/frmUrlLink.cs
+++ KairosChallenge/frmUrlLink.cs
@@ -12,9 +12,17 @@ namespace KairosChallenge
 {
     public partial class frmUrlLink : Form
     {
+        DecodeURL BuildUrl = new DecodeURL();
         public frmUrlLink()
         {
             InitializeComponent();
+
+            infoTxt.Text = "Si pulsas el botón te llevara a la URL: " + BuildUrl.URL;
+        }
+
+        private void urlBtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(BuildUrl.URL);
         }
     }
 }
